825621f8969ca99f37644a8f2b65b79798aa7d8d
Lennon <lennonmilicic@gmail.com>
2020-05-18 19:21:36 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# aws-s3
+# @dieover/aws-s3
+
+```js
+const AWSS3 = require('@dieover/aws-s3');
+
+const aws = new AWSS3({
+    ACCESS_KEY_ID: '',
+    SECRET_ACCESS_KEY: '',
+    S3_REGION: '',
+    S3_BUCKET: '',
+    S3_VERSION: ''
+});
+const image = await aws.upload('data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQAB...', 'images', 'new');
+const deleted = await aws.delete(image.replace('https://S3_BUCKET.s3.S3_REGION.amazonaws.com/', ''));
+```
+
+## Upload
+
+- **BASE64**: data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQAB...
+- **FOLDER**: images/something/else
+    - whitout "/" in start and end of string
+- **NAME**: filename
+    - without extension
+- **SUCCESS RETURN**: Photo URL.
+- **FAILED RETURN**: Failed to save image.
+```
+aws.upload(base64, folder, name);
+```
+
+## Delete
+
+- **URL**: https://S3_BUCKET.s3.S3_REGION.amazonaws.com/images/new.png
+    - Juste replace domain to get key image of S3.
+- **SUCCESS RETURN**: Image Deleted.
+- **FAILED RETURN**: Failed to delete image.
+```
+aws.delete(key);
+```
---
+++ index.js
@@ -0,0 +1,86 @@
+const AWS = require('aws-sdk');
+
+class AWSS3 {
+
+    _AWS_CONFIG = {
+        SECRET_ACCESS_KEY: null,
+        ACCESS_KEY_ID: null,
+        S3_VERSION: null,
+        S3_BUCKET: null,
+        S3_REGION: null
+    };
+
+    /**
+     * @param {Object} config Configuration of AWSS3
+     * @param {Object} config.ACCESS_KEY_ID Access Key of AWS
+     * @param {Object} config.SECRET_ACCESS_KEY Secret Key of AWS
+     * @param {Object} config.S3_VERSION Version of AWS S3
+     * @param {Object} config.S3_BUCKET Bucket of AWS S3
+     * @param {Object} config.S3_REGION Region of AWS S3
+     */
+    constructor(config) {
+        this._AWS_CONFIG = config;
+    }
+
+    /**
+     * This function upload de base64 in a converted PNG file and return de URL direct of the image on AWS S3
+     * @param {string} base64 Base64 of Image
+     * @param {string} folder Folder name to save Image e.g: 'imagens/perfils'
+     * @param {string} fileName Filename of Image whiout extension (all base64 are converted to png)
+     */
+    async upload(base64, folder, fileName) {
+        try {
+            const s3 = new AWS.S3({
+                accessKeyId: this._AWS_CONFIG.ACCESS_KEY_ID,
+                secretAccessKey: this._AWS_CONFIG.SECRET_ACCESS_KEY,
+                region: this._AWS_CONFIG.S3_REGION,
+                apiVersion: this._AWS_CONFIG.S3_VERSION
+            });
+            const base64Data = Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+            const params = {
+                Bucket: this._AWS_CONFIG.S3_BUCKET,
+                Key: `${folder}/${fileName}.png`,
+                Body: base64Data,
+                ACL: 'public-read',
+                ContentEncoding: 'base64',
+                ContentType: `image/png`,
+            };
+            const result = (await s3.upload(params).promise()).Location;
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject('Failed to save image.');
+        }
+    }
+
+    /**
+     * This function delete file on AWS S3
+     * Key is the URL of File whiout de domain
+     * e.g:
+     * 
+     * URL: https://S3_BUCKET.s3.S3_REGION.amazonaws.com/images/new.png
+     * 
+     * KEY Will be: images/new.png
+     * @param {string} key Key of file to delete
+     */
+    async delete(key) {
+        try {
+            const s3 = new AWS.S3({
+                accessKeyId: this._AWS_CONFIG.ACCESS_KEY_ID,
+                secretAccessKey: this._AWS_CONFIG.SECRET_ACCESS_KEY,
+                region: this._AWS_CONFIG.S3_REGION,
+                apiVersion: this._AWS_CONFIG.S3_VERSION
+            });
+            const params = {
+                Bucket: this._AWS_CONFIG.S3_BUCKET,
+                Key: key
+            };
+            await s3.deleteObject(params).promise();
+            return Promise.resolve('Image Deleted.');
+        } catch (error) {
+            return Promise.reject('Failed to delete image.');
+        }
+    }
+
+}
+
+module.exports = AWSS3;
